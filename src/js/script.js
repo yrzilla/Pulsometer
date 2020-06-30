ad0097ed04e02d2d7b1e30f4667ed0b7c189ff5f
YRZILLA <xbaracusx@mail.ru>
2020-06-30 12:57:02 +0300

    Добавил валидацию форм

--- src/js/script.js
+++ src/js/script.js
@@ -46,4 +46,35 @@ $(document).ready(function(){
         $('.overlay, #order').fadeIn('slow');
       })
     });
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name:{
+            required:"Пожалуйста введите своё имя",
+            minlength: jQuery.validator.format("Введите {0} символов!")
+          },
+          phone:"Пожалуйста введите свой телефон",
+          email:{
+            required: "Пожалуйста введите свою почту",
+            email: "Неправильно введён адрес почты"
+          }
+        }
+      });
+    };
+    validateForms("#consultation-form");
+    validateForms("#order form");
+    validateForms("#consultation form");
+
 });
